a49bc03ff6749659f92e82d1bf3829dc9953067c
vshelke <vaibhavshelke017@gmail.com>
2024-03-30 15:20:06 +0530

    feat: adds app skeleton

---
+++ app/src/pages/_app.tsx
@@ -0,0 +1,44 @@
+import "@/styles/globals.css";
+import type { ReactElement, ReactNode } from "react";
+import type { NextPage } from "next";
+import type { AppProps } from "next/app";
+import { Inter as FontSans } from "next/font/google";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { Toaster } from "@/components/ui/sonner"
+
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+});
+
+function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? ((page) => page);
+  return (
+    <>
+      <style jsx global>{`
+        :root {
+          --font-sans: ${fontSans.variable};
+        }
+      `}</style>
+      <main className="min-h-screen bg-background font-sans antialiased">
+        <TooltipProvider>
+          {getLayout(<Component {...pageProps} />)}
+        </TooltipProvider>
+        <Toaster />
+      </main>
+    </>
+  );
+}
+
+export default App;
