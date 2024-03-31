22324f8cb5a861bd4ab392125075539a280ff79b
vshelke <vaibhavshelke017@gmail.com>
2024-03-31 17:35:53 +0530

    format: black

--- api/ecom/urls.py
+++ api/ecom/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import ProductCategoryListView, ProductListView, ProductDetailView
+from .views import ProductCategoryListView, ProductDetailView, ProductListView
 
 urlpatterns = [
     path("products/", ProductListView.as_view(), name="product-list"),
